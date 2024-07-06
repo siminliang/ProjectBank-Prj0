a85588861815c6b42b7c3344723fbd6a3ecb9790
Tina Liang <tinaliang129@gmail.com>
2024-07-05 20:51:36 -0700

    start

--- src/main/java/com/revature/Main.java
+++ src/main/java/com/revature/Main.java
@@ -1,4 +1,22 @@
 package com.revature;
 
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args) {
+
+        try(Scanner scanner = new Scanner(System.in)){
+            //UserDAO handle access persisting User data
+
+
+            //UserService handle validating User data and follow software + business rules
+            //UserService needs access to DAO to transfer data to repo layer
+
+
+            //UserController handle receiving and returning data to the user
+            //UserController needs access to service to transfer data to service layer
+
+
+        }
+    }
 }
---
+++ src/main/java/com/revature/controller/UserController.java
@@ -0,0 +1,16 @@
+package com.revature.controller;
+
+import java.util.Scanner;
+
+public class UserController {
+    private Scanner scanner;
+
+    public UserController(Scanner scanner){
+        this.scanner = scanner;
+    }
+
+    public void getServiceController(){
+        System.out.println("What would you like to do today?");
+
+    }
+}
